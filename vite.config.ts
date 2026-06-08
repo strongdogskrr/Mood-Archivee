faeafe3adb3195c110fddc8fe3554d2c21abe187
strongdogskrr <hiktf0212@naver.com>
2026-06-09 02:30:02 +0900

    build: set base path for deployment
    Configure the base URL to support deployment on GitHub Pages.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Mood-Archivee/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
